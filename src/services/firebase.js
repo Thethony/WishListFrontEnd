de85f03a7d6d47cb8ccfd17981f4b7584914bc44
Thethony <anthonyyimlee@gmail.com>
2021-11-02 18:34:41 -0400

    remade folders and repo because firebase version couldn't install

---
+++ src/services/firebase.js
@@ -0,0 +1,30 @@
+import firebase from 'firebase/app';
+import 'firebase/auth'
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAL2R-e0VI8GgU61M1ooU7j-pN-GFjGRSM",
+    authDomain: "wishlist-1384b.firebaseapp.com",
+    projectId: "wishlist-1384b",
+    storageBucket: "wishlist-1384b.appspot.com",
+    messagingSenderId: "840532688895",
+    appId: "1:840532688895:web:8b686d3b4bba6cb863b1ca"
+  };
+
+  firebase.initializeApp(firebaseConfig);
+
+  const auth = firebase.auth();
+  const provider = new firebase.auth.GithubAuthProvider
+
+  function signIn() {
+      return auth.signInWithPopup(provider);
+  }
+
+  function logOut() {
+      return auth.signOut();
+  }
+
+  export {
+      auth,
+      signIn,
+      logOut
+  }
