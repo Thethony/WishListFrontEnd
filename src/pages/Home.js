343dd20269318ce5e34872fdbbb8eff8deb5e0e9
Thethony <anthonyyimlee@gmail.com>
2021-11-03 17:00:39 -0400

    got data to show with create/delete/update but need to work on making the functionality work and app not crash

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,71 @@
+import { Link } from "react-router-dom"
+import { useState } from 'react'
+
 const Home = (props) => {
+    const [newform, setNewForm] = useState(getNewState());
+    const loaded = () => {
+        return props.wish.map(wishList => ( 
+            <div key={wishList._id} className="wishList">
+                <Link to={`/wish/${wishList._id}`}>
+                    <h1>{wishList.product}</h1>
+                </Link>
+                <img style={{height: 150, width: 100,}} src={wishList.image} alt={wishList.product} />
+                <h3>{wishList.price}</h3>
+            </div>
+        ))
+    }
+
+const loading = () => <h1>Loading ...</h1>
+
+const handleChange = (event) => {
+
+    setNewForm(prevState => ({
+                ...prevState,
+                [event.target.product]: event.target.value 
+    }))
+}
+
+const handleSubmit = (event) => {
+    event.preventDefault();
+    props.createWish(newform)
+    setNewForm(getNewState())
+}
+
+function getNewState() {
+    return {
+        product: "",
+        image: "",
+        price: "",
+    }
+}
     return (
-        <home>
-            <title>Welcome To Wish List</title>
-        </home>
+        <section>
+            <form className="Form" onSubmit={handleSubmit}>
+                <input
+                    value={newform.product}
+                    onChange={handleChange}
+                    text="text"
+                    placeholder="Product Name"
+                    name="product"
+                />
+                <input
+                    value={newform.image}
+                    onChange={handleChange}
+                    type="url"
+                    placeholder="Product image"
+                    name="image"
+                />
+                    <input
+                    value={newform.price}
+                    onChange={handleChange}
+                    text="text"
+                    placeholder="Product Price"
+                    name="price"
+                />
+                <input type="submit" value="Create Person" />
+            </form>
+            { props.wish ? loaded() : loading() }
+        </section>
     )
 }
 
